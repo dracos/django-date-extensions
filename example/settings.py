2e55d0b27f450b6bd42870718dc61252025a6712
Edmund von der Burg <evdb@mysociety.org>
2013-04-09 12:34:13 +0100

    Enable running of tests using `python example/manage.py test`

--- example/settings.py
+++ example/settings.py
@@ -1,3 +1,13 @@
 SECRET_KEY = 'x9i*)8dp_i=r1o(p%0-g*^sz@_(631+_tjr=e-t04vj2!@l$8a'
 ROOT_URLCONF = 'example.urls'
-INSTALLED_APPS = ( 'app', )
+INSTALLED_APPS = (
+    'app',
+    'django_date_extensions',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'example.sqlite3',
+    }
+}
