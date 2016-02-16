1f20aa9b6c721e4b10b7a5fed490d34c0b1bc4ef
Mark Longair <mhl@pobox.com>
2016-02-16 11:20:08 +0000

    Bump the version number prior to uploading the new version to pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django_date_extensions',
-    version='0.1dev',
+    version='0.2dev',
     packages=['django_date_extensions',],
     license='GNU Affero General Public license',
     long_description=open('README.txt').read(),
