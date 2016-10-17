f0369409dbd3ece8843e68f2e0d60e4ef64abfc3
intellisense <eye-see-you@live.com>
2016-10-18 00:27:15 +0500

    prefix year dates

--- django_date_extensions/settings.py
+++ django_date_extensions/settings.py
@@ -31,3 +31,6 @@ DAY_MONTH_INPUT_FORMATS = getattr(settings, 'DATE_EXTENSIONS_DAY_MONTH_INPUT_FOR
     '%b %d', '%d %b',  # 'Oct 25', '25 Oct'
     '%B %d', '%d %B',  # 'October 25', '25 October'
 ))
+
+ALLOWED_PREFIX = [p.lower() for p in getattr(settings, 'DATE_EXTENSIONS_ALLOWED_PREFIX', [])]
+
