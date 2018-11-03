c1409bc8454be40ca32af3b1b489498500cfc113
Frank Sachsenheim <funkyfuture@riseup.net>
2018-11-03 17:51:43 +0100

    Fixes the example app's settings and simplifies .gitignore

--- example/settings.py
+++ example/settings.py
@@ -1,6 +1,5 @@
+from os import path
 
-# Set DEBUG to true so that we don't need to provide a 500.html template
-# (django's debug on will be used instead).
 DEBUG = True
 
 SECRET_KEY = 'x9i*)8dp_i=r1o(p%0-g*^sz@_(631+_tjr=e-t04vj2!@l$8a'
@@ -8,6 +7,22 @@ ROOT_URLCONF = 'example.urls'
 INSTALLED_APPS = (
     'django_date_extensions',
 )
+
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [path.abspath(path.join(path.dirname(__file__), 'templates'))],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+            ]
+        },
+    }
+]
+
 
 DATABASES = {
     'default': {
