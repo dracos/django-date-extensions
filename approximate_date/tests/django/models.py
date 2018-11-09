296d234b47edae534f008e9b77917105034117ee
Frank Sachsenheim <funkyfuture@riseup.net>
2018-11-09 01:19:25 +0100

    Removes a Python 2 relevant chunk

--- approximate_date/tests/django/models.py
+++ approximate_date/tests/django/models.py
@@ -6,6 +6,3 @@ from approximate_date.django.fields import VagueDateField
 class TestModel(Model):
     start = VagueDateField()
     can_be_null = VagueDateField(null=True)
-
-    def __unicode__(self):
-        return u'%s' % str(self.start)
